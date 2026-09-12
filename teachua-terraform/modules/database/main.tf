d2d94b2a9a92cf76fb39bb91d82a35693b128711
Dmytro Simko <dima70593@ukr.net>
2026-09-12 11:11:42 +0000

    Isolated databases

--- teachua-terraform/modules/database/main.tf
+++ teachua-terraform/modules/database/main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "rds" {
     from_port       = 3306
     to_port         = 3306
     protocol        = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    security_groups = [aws_security_group.ecs.id]  
   }
   egress {
     from_port   = 0
